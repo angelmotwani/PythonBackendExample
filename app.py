eeee4a10651e2f02bb16741aebe7e1e7adae44a4
Angel <motwaniangel7@gmail.com>
2026-05-03 14:23:28 +0530

    Creating Repo

---
+++ app.py
@@ -0,0 +1,121 @@
+from flask import Flask, jsonify, request
+import sqlite3
+import hashlib
+app= Flask(__name__)
+
+def get_db_connection():
+     import os
+     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+     db_path=os.path.join(BASE_DIR, "products.db")
+     conn= sqlite3.connect(db_path)
+     conn.row_factory = sqlite3.Row
+     return conn 
+
+@app.route("/init", methods=["GET"])
+def init_db():
+     conn = get_db_connection()
+     conn.execute('''
+                  CREATE TABLE IF NOT EXISTS products(
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  name TEXT NOT NULL,
+                  price REAL NOT NULL
+                  )
+                  ''')
+     
+     conn.execute('''
+                  CREATE TABLE IF NOT EXISTS users(
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  username TEXT UNIQUE NOT NULL,
+                  password TEXT NOT NULL
+                  )
+                  ''')
+     conn.commit()
+     conn.close()
+     return jsonify({"message": "database init complete"})
+
+     
+
+
+@app.route("/")
+def home():
+    return jsonify({"message":"hello from our first flask server!"})
+
+
+
+@app.route("/products", methods=["GET"])
+def get_products():
+       conn= get_db_connection()
+       rows= conn.execute("SELECT * FROM products").fetchall()
+       conn.close()
+       return jsonify([dict(row) for row in rows])
+    
+
+@app.route("/products", methods=["POST"])
+def add_products():
+    data= request.get_json()
+    name= data.get("name")
+    price= data.get("price")
+    conn= get_db_connection()
+    cursor= conn.cursor()
+    cursor.execute('INSERT INTO products (name, price) VALUES (?,?)', (name, price))
+    conn.commit()
+    new_id= cursor.lastrowid
+    conn.close()
+
+
+
+  
+    new_product = {
+          "id": new_id,
+          "name": name,
+          "price": price
+
+     }
+    
+    return jsonify({"message":"product has been added", "product": new_product}), 201
+
+@app.route("/register", methods=["POST"])
+def register():
+    data= request.get_json()
+    username= data.get("username")
+    password= data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "missing username or password"}), 400
+     
+    hashed_password= hashlib.sha256(password.encode()).hexdigest()
+
+    try:
+        conn=get_db_connection()
+        conn.execute("INSERT INTO USERS (username, password) VALUES (?,?)" ,(username, hashed_password))
+        conn.commit()
+        conn.close()
+        return jsonify({"message": "user registered sucessfully"}), 201
+    except sqlite3.IntegrityError:
+        return jsonify({"message":"username already exists"}), 400
+
+
+@app.route("/login", methods=["POST"])
+def login():
+     data= request.get_json()
+     username= data.get("username")
+     password= data.get("password")
+
+     if not username or not password:
+          return jsonify({"message": "missing username or password"})
+     
+     hashed_password= hashlib.sha256(password.encode()).hexdigest()
+     conn= get_db_connection()
+     user= conn.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password)). fetchone()
+     conn.close()
+
+     if user:
+          return jsonify({"message": f"Welcome {username}!"})
+     else:
+          return jsonify({"error": "invalid credentials"}), 401
+        
+
+if __name__ == "__main__":
+    with app.app_context():
+         init_db()
+    app.run(debug=True)
